8b5b582449863a87437537c1bf1838c48568ba94
Aditya Joshi <144603349+omicron9009@users.noreply.github.com>
2025-06-11 10:27:17 +0530

    Time: 0 ms (100%), Space: 13 MB (15.07%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.cpp
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -2,21 +2,40 @@ class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
         
-        for(int n = 0 ; n < nums1.size();n++){
-            bool found=false;
-            auto idx = find(nums2.begin(),nums2.end(),nums1[n]);
-            int index=idx-nums2.begin();
-            for(int i = index+1 ; i < nums2.size();i++){
-                if(nums2[i]>nums1[n]){
-                    nums1[n]=nums2[i];
-                    found=true;
-                    break;
-                }
+        // for(int n = 0 ; n < nums1.size();n++){
+        //     bool found=false;
+        //     auto idx = find(nums2.begin(),nums2.end(),nums1[n]);
+        //     int index=idx-nums2.begin();
+        //     for(int i = index+1 ; i < nums2.size();i++){
+        //         if(nums2[i]>nums1[n]){
+        //             nums1[n]=nums2[i];
+        //             found=true;
+        //             break;
+        //         }
+        //     }
+        //     if(!found){
+        //         nums1[n]=-1;
+        //     }
+        // }
+        // return nums1;
+
+        // using monotonic stack !
+        unordered_map<int , int> map;
+        stack<int> st;
+
+        for(int i = nums2.size()-1;i>=0;--i){
+            int num = nums2[i];
+            while(!st.empty() && st.top() <= num){
+                st.pop();
             }
-            if(!found){
-                nums1[n]=-1;
-            }
+            map[num]=st.empty()?-1: st.top();
+            st.push(num);
         }
-        return nums1;
+        vector<int> res ; 
+        for(int num : nums1){
+            res.push_back(map[num]);
+        }
+        return res;
+
     }
 };
