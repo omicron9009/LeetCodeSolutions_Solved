19dd4304de0f256a2c503bf74cec2edb9192f838
Aditya Joshi <144603349+omicron9009@users.noreply.github.com>
2025-06-12 10:14:03 +0530

    Time: 25 ms (82.76%), Space: 71 MB (79.06%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -1,13 +1,31 @@
 class Solution {
 public:
     bool containsDuplicate(vector<int>& arr) {
-        unordered_map<int , int> map ; 
-        for(int n: arr){
-            map[n]++;
-        }
-        for(auto &kv : map){
-            if(kv.second >1) return true;
+        // unordered_map<int , int> map ; 
+        // for(int n: arr){
+        //     map[n]++;
+        // }
+        // for(auto &kv : map){
+        //     if(kv.second >1) return true;
+        // }
+        // return false;
+
+
+        // unordered_set<int> st;
+        // for(int n : arr){
+        //     if(st.find(n)!=st.end()){
+        //         return true;
+        //     }
+        //     st.insert(n);
+        // }
+        // return false;
+
+        sort(arr.begin(),arr.end());
+        for(int i = 0 ; i < arr.size()-1 ; i++){
+            if(arr[i]==arr[i+1]) return true;
         }
         return false;
+
+
     }
 };
