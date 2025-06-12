4fef5cd013f00d05abc664c28d555d9a07e4f56a
Aditya Joshi <144603349+omicron9009@users.noreply.github.com>
2025-06-12 10:37:01 +0530

    Time: 0 ms (100%), Space: 21.8 MB (70.37%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -2,12 +2,19 @@ class Solution {
 public:
     int missingNumber(vector<int>& arr) {
 
-        sort(arr.begin(),arr.end());
+        // sort(arr.begin(),arr.end());
 
-        for(int i = 0 ;i<arr.size();i++){
-            if(arr[i]!=i)return i;
+        // for(int i = 0 ;i<arr.size();i++){
+        //     if(arr[i]!=i)return i;
+        // }
+        // return arr.size();
+
+        int sum_upto = (arr.size())*(arr.size()+1)/2;
+        int sum=0;
+        for(int i=0 ; i < arr.size() ; i++){
+            sum+=arr[i];
         }
-        return arr.size();
+        return sum_upto-sum;
         
     }
 };
